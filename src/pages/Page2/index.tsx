3bf69f2c08f01ff52257edc28046782e7b02e821
Henrique <h.rodrigues@runtime-revolution.com>
2025-02-14 16:18:27 +0000

    Adds Functionality of the @std

--- src/pages/Page2/index.tsx
+++ src/pages/Page2/index.tsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react'
-import { toCamelCase, compareSimilarity } from 'jsr:@std/text';
+import { useState } from 'react'
+import { toCamelCase, compareSimilarity, toSnakeCase, toPascalCase, toKebabCase } from 'jsr:@std/text';
 
+const DEMO_TEXT = 'Hello world im a demo text that is being used with Deno @std'
 export default function Page2() {
   const [currentCompare, setCurrentCompare] = useState('Hom')
-  const [isCamelCase, setIsCamelCase] = useState(false)
+  const [textCase, setTextCase] = useState('camel')
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => { setTextCase(e.target.value) }
 
   const result = [
     'Home',
@@ -14,22 +17,40 @@ export default function Page2() {
     'Expert',
   ].sort(compareSimilarity(currentCompare))
 
+  let displayText = DEMO_TEXT
+  switch (textCase) {
+    case 'camel':
+      displayText = toCamelCase(DEMO_TEXT)
+      break
+    case 'pascal':
+      displayText = toPascalCase(DEMO_TEXT)
+      break
+    case 'snake':
+      displayText = toSnakeCase(DEMO_TEXT)
+      break
+    case 'kebab':
+      displayText = toKebabCase(DEMO_TEXT)
+      break
+    default:
+      break;
+  }
+
   return (
     <div>
       <h2>Page2</h2>
 
-      <p>
-        This is the Page2 page. Check browser console to see the output of the Deno read file operation.
-      </p>
-
       <div>
         <div>
-          <div>
-            Convert to camelCase
-            <input type='checkbox' checked={isCamelCase} onChange={(e) => setIsCamelCase(e.target.checked)} />
-          </div>
+          <p>Convert to camelCase</p>
 
-          {isCamelCase ? toCamelCase('hello world') : 'hello world'}
+          <select value={textCase} onChange={handleSelect}>
+            <option value='camel'>camel</option>
+            <option value='pascal'>pascal</option>
+            <option value='snake'>snake</option>
+            <option value='kebab'>kebab</option>
+          </select>
+
+          <p>{displayText}</p>
         </div>
 
         <div>
